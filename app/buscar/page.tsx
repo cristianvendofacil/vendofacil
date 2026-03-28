d2d14b05ebcf0403d09b054653ba4e5a4deb6c77
Cristian Reta <cristian1reta@gmail.com>
2026-03-28 15:42:14 +0100

    fix final prerender buscar

--- app/buscar/page.tsx
+++ app/buscar/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 export const revalidate = 0;
 import { useEffect, useMemo, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
